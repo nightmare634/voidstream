bb81435cdf0883f06d7ea21b4d5abd102247e810
Ajanlekoko11 <risagencyy@gmail.com>
2026-01-02 16:18:53 +0100

    Polyfill Buffer for browser

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,21 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
-export default defineConfig({ plugins: [tailwindcss(), sveltekit()] });
+export default defineConfig({
+	plugins: [
+		tailwindcss(),
+		sveltekit(),
+		nodePolyfills({
+			globals: {
+				Buffer: true,
+				global: true,
+				process: true
+			}
+		})
+	],
+	define: {
+		global: 'globalThis'
+	}
+});
